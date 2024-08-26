b7aaf3c2c93c1cdea7cdd5c725b9ba3f72971776
chyuan-cuihongyuan <1723037666@qq.com>
2024-08-27 00:02:51 +0800

    feat: init project

---
+++ src/main/java/cn/chyuan/ltzf/utils/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author cuihongyuan
+ * @Date: 2024/8/27 0027  - 0:01
+ * @Description: cn.chyuan.ltzf.utils
+ * @version: 1.0
+ */
+package cn.chyuan.ltzf.utils;
---
+++ src/test/java/cn/chyuan/ltzf/test/ApiTest.java
@@ -0,0 +1,11 @@
+package cn.chyuan.ltzf.test;
+
+/**
+ * @className: ApiTest
+ * @author: cuihongyuan chyuan.cn @chyuan
+ * @date: 2024/8/27 0027 0:00
+ * @Version: 1.0
+ * @description:
+ */
+public class ApiTest {
+}
